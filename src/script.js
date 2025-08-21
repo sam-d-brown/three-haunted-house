486ea92e26a20437adca40d74a2ebe20b02121c7
Sam Brown <sam@madebysix.com>
2025-08-21 16:10:26 +0100

    feat(fog): add fog

--- src/script.js
+++ src/script.js
@@ -379,6 +379,12 @@ sky.material.uniforms["mieDirectionalG"].value = 0.95;
 sky.material.uniforms["sunPosition"].value.set(0.3, -0.038, -0.95);
 
 /**
+ * fog
+ */
+
+scene.fog = new THREE.FogExp2("#04343f", 0.1);
+
+/**
  * Animate
  */
 const timer = new Timer();
